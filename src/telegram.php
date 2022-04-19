478a7197ffad6ff42891e8b6bbe7cce344c9d6e2
Protocol Corporation <protocollive@outlook.com>
2022-04-19 12:50:49 -0300

    Funções do Telegram

---
+++ src/telegram.php
@@ -0,0 +1,47 @@
+<?php
+//2022.04.19.00
+
+function TelegramSignCheck(string $Token, array $Data):bool{
+  if(isset($Data['username'])
+  and $Data['username'] === 'undefined'):
+    unset($Data['username']);
+  endif;
+  if($Data['last_name'] === 'undefined'):
+    unset($Data['last_name']);
+  endif;
+  $HashSended = $Data['hash'];
+  unset($Data['hash']);
+  $array = [];
+  foreach($Data as $key => $value):
+    $array[] = $key . '=' . $value;
+  endforeach;
+  sort($array);
+  $array = implode("\n", $array);
+  $Token = hash('sha256', $Token, true);
+  $HashRight = hash_hmac('sha256', $array, $Token);
+  if(strcmp($HashSended, $HashRight) !== 0
+  or time() > strtotime('+5 minutes', $Data['auth_date'])):
+    return false;
+  else:
+    return true;
+  endif;
+}
+
+function TelegramWebappCheck(string $Token, array $Data):bool{
+  $HashSended = $Data['hash'];
+  unset($Data['hash']);
+  $array = [];
+  foreach($Data as $key => $value):
+    $array[] = $key . '=' . $value;
+  endforeach;
+  sort($array);
+  $array = implode("\n", $array);
+  $Token = hash_hmac('sha256', $Token, 'WebAppData', true);
+  $HashRight = hash_hmac('sha256', $array, $Token);
+  if($HashSended !== $HashRight
+  or time() > strtotime('+5 minutes', $Data['auth_date'])):
+    return false;
+  else:
+    return true;
+  endif;
+}
