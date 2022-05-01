ae14a8fb8339934f3b6ab47a5dc1c5ca4b6ab93d
Protocol Corporation <protocollive@outlook.com>
2022-05-01 11:11:58 -0300

    Melhoria no AccentInsensitive

--- src/basics.php
+++ src/basics.php
@@ -1,16 +1,10 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/FuncoesComuns
-//2022.04.27.00
+//2022.05.01.00
 
 function AccentInsensitive(string $Text):string{
-  return strtr($Text, [
-    'ã' => 'a',
-    'á' => 'a',
-    'à' => 'a',
-    'ä' => 'a',
-    'â' => 'a'
-  ]);
+  return iconv('utf-8', 'ascii//TRANSLIT', $Text);
 }
 
 function Equals(string $Text1, string $Text2):bool{
