af226a514f70864cff9f6b05ccc210eb75aed106
Protocol Corporation <protocollive@outlook.com>
2022-10-27 07:32:31 -0300

    Corrigido os dias da semana

--- src/basics.php
+++ src/basics.php
@@ -1,16 +1,16 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/FuncoesComuns
-//2022.10.21.05
+//2022.10.27.00
 
 enum DiaSemana:int{
-  case Domingo = 0;
+  case Domingo = 7;
   case Segunda = 1;
   case Terca = 2;
-  case Quarta = 4;
-  case Quinta = 5;
-  case Sexta = 6;
-  case Sabado = 7;
+  case Quarta = 3;
+  case Quinta = 4;
+  case Sexta = 5;
+  case Sabado = 6;
 }
 
 function AccentInsensitive(string $Text):string{
