a0df9727068ec209da1ce38e77fae18863023dfa
Protocol Corporation <protocollive@outlook.com>
2023-09-06 15:55:10 -0300

    Uning functions

--- src/debug.php
+++ src/debug.php
@@ -1,43 +1,33 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/FuncoesComuns
-//2023.08.02.00
+//2023.09.06.00
 
-function HandlerError(
-  int $errno,
-  string $errstr,
-  string $errfile = null,
-  int $errline = null,
-  array $errcontext = null
-):never{
-  DebugTrace();
-  ob_start();
-  echo date('Y-m-d H:i:s') . ' ' . microtime(true) . PHP_EOL;
-  echo 'Error #' . $errno . ' - ' . $errstr . ' in ' . $errfile . ' (' . $errline . ')' . PHP_EOL;
-  debug_print_backtrace();
-  $log = ob_get_contents();
-  ob_end_clean();
-  error_log($log);
-  if(PHP_SAPI !== 'cli' and ini_get('display_errors')):
-    echo '<pre style="text-align:left">' . $log . '</pre>';
-  endif;
-  die();
-}
-
-function HandlerException(
-  Throwable $Exception
+function Handler(
+  mixed ...$Args
 ):void{
   DebugTrace();
   ob_start();
   echo date('Y-m-d H:i:s') . ' ' . microtime(true) . PHP_EOL;
-  echo 'Exception: ';
-  var_dump($Exception);
-  debug_print_backtrace();
+  $type = func_get_arg(0);
+  if(is_int($type)):
+    echo 'Error #' . $type . ' - ' . func_get_arg(1) . PHP_EOL .
+      func_get_arg(2) . ' (' . func_get_arg(3) . ')' . PHP_EOL;
+    debug_print_backtrace();
+  else:
+    echo 'Exception: ' . $type->getMessage() . PHP_EOL .
+      $type->getFile() . ' (' . $type->getLine() . ')' . PHP_EOL;
+    var_dump($type);
+  endif;
   $log = ob_get_contents();
   ob_end_clean();
   error_log($log);
-  if(PHP_SAPI !== 'cli' and ini_get('display_errors')):
-    echo '<pre style="text-align:left">' . $log . '</pre>';
+  if(ini_get('display_errors')):
+    if(PHP_SAPI === 'cli' or ini_get('html_errors') == false):
+      echo $log;
+    else:
+      echo '<pre style="text-align:left">' . $log . '</pre>';
+    endif;
   endif;
   die();
 }
