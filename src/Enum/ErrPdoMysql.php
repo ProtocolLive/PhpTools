70178ca8b358656dae5ac2c43cdf4598095cb031
Protocol Corporation <protocollive@outlook.com>
2026-03-10 16:45:00 -0300

    New code

--- src/Enum/ErrPdoMysql.php
+++ src/Enum/ErrPdoMysql.php
@@ -3,11 +3,12 @@
 //https://github.com/ProtocolLive/PhpTools
 
 /**
- * @version 2025.05.26.00
+ * @version 2026.03.10.00
  */
 enum ErrPdoMysql:string{
   case ColumnNotFound = '42S22';
   case IndexNotFound = 'HY000';
   case Permissions = '42000';
   case TableNotFoud = '42S02';
+  case IntegrityViolation = '23000';
 }
