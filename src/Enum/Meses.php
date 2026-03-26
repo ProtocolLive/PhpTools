b59dd69c193539d47a815b3ca61a986b9adc27d8
Protocol Corporation <protocollive@outlook.com>
2026-03-26 13:14:56 -0300

    New enum

---
+++ src/Enum/Meses.php
@@ -0,0 +1,21 @@
+<?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive/PhpTools
+
+/**
+ * @version 2026.03.26.00
+ */
+enum Meses:int{
+  case Janeiro = 1;
+  case Fevereiro = 2;
+  case Março = 3;
+  case Abril = 4;
+  case Maio = 5;
+  case Junho = 6;
+  case Julho = 7;
+  case Agosto = 8;
+  case Setembro = 9;
+  case Outubro = 10;
+  case Novembro = 11;
+  case Dezembro = 12;
+}
