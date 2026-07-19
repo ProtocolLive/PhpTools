69c24d3a612039cb239074847f7bb3cd1ffbf9de
Protocol Corporation <protocollive@outlook.com>
2026-07-19 08:44:18 -0300

    Added weekday in Portuguese

---
+++ src/Enum/Semana.php
@@ -0,0 +1,16 @@
+<?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive/PhpTools
+
+/**
+ * @version 2024.07.19.00
+ */
+enum Semana:int{
+  case Domingo = 0;
+  case Segunda = 1;
+  case Terca = 2;
+  case Quarta = 3;
+  case Quinta = 4;
+  case Sexta = 5;
+  case Sabado = 6;
+}
