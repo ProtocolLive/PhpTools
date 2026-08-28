d8cb1242bcfea7c95ec564b8b89d18df46008347
Protocol Corporation <protocollive@outlook.com>
2026-08-28 15:20:03 -0300

    New error

--- src/Enum/ErrPdoMysql.php
+++ src/Enum/ErrPdoMysql.php
@@ -3,12 +3,12 @@
 //https://github.com/ProtocolLive/PhpTools
 
 /**
- * @version 2026.03.29.00
+ * @version 2026.08.04.00
  */
 enum ErrPdoMysql:string{
   case ColumnNotFound = '42S22';
   case IndexNotFound = 'HY000';
-  case Permissions = '42000';
+  case PermissionsSintax = '42000';
   case TableNotFoud = '42S02';
   case IntegrityViolation = '23000';
   case LockTime = '1205';
